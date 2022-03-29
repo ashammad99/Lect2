267705bfbcdbb17105c98f26ff53702c3726aac6
ashammad99 <45128738+ashammad99@users.noreply.github.com>
2022-03-29 23:05:05 +0300

    first Commit 29/3/2022

---
+++ lect2.php
@@ -0,0 +1,8 @@
+<?php
+    $x = 5;
+    $y = 7.5;
+    $name = "Ahmed Salah Ahmed hammad";
+    echo gettype($x);
+    echo gettype($y);
+     var_dump($name);
+    //print_r($GLOBALS);
---
+++ table.php
@@ -0,0 +1,11 @@
+<?php
+    $name = "Ahmed Hammad";
+    $dept = "SDEV";
+    $avg = 91.9;
+    
+    echo "<table border='1'>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Department</th>";
+    echo "<th>Average</th></tr>";
+    echo "<tr><td>$name</td><td>$dept</td><td>$avg</td></tr>";
+    echo "</table>";
